8463134a939bca83ac6fbaf15416734946d3ffaa
Buce-Ithon <78635197+Buce-Ithon@users.noreply.github.com>
2026-05-21 15:38:42 +0800

    Version 1.0.0 Unlimited number of times the vault can be opened in Trial Chambers.

---
+++ src/client/java/com/supernoob/vanillaopt/client/VanillaOptDataGenerator.java
@@ -0,0 +1,11 @@
+package com.supernoob.vanillaopt.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillaOptDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
